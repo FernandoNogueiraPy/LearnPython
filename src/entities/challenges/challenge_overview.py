ce9cda011be7b174430cabf80aaadcf6d6a9f43e
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-09-04 11:56:41 -0300

    feat: Add ChallengeOverview model and related classes
    This commit adds the ChallengeOverview model and related classes to the `challenge_overview.py` file. The ChallengeOverview model includes fields such as `id_player`, `challenge_info`, and `challenge_history`, which represent the player's ID, challenge information, and challenge history respectively. The related classes, such as `ChallengeInfo`, `HistoryChallengeCurrent`, and `ModeChallengeHistory`, provide additional details for the challenge overview. This addition enhances the functionality of the application by providing a structured representation of the challenge overview data.

---
+++ src/entities/challenges/challenge_overview.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+from src.entities.maps.mapas import MAPS_NAME
+
+
+class ChallengeInfo(BaseModel):
+    total_challenges: int = 0
+    total_points: int = 0
+    total_completed: int = 0
+    total_failed: int = 0
+
+
+class HistoryChallengeCurrent(BaseModel):
+    id_challenge: int = 1
+    challenge_name: str = "Desafio 1"
+    challenge_points: int = 0
+    challenge_completed: bool = False
+
+
+class ModeChallengeHistory(BaseModel):
+    maps_total: int = 5
+    maps_completed: int = 0
+    maps_peding: int = 5
+    map_current: int = 1
+    map_current_name: MAPS_NAME = "Vale das Variaveis"
+    history_current: HistoryChallengeCurrent = HistoryChallengeCurrent()
+
+
+class ChallengeOverview(BaseModel):
+    id_player: str
+    challenge_info: ChallengeInfo = ChallengeInfo()
+    challenge_history: ModeChallengeHistory = ModeChallengeHistory()
