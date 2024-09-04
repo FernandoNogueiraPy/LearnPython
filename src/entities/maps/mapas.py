5da9b3152ee69a019c548af2d3925feab3842372
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-09-04 11:56:35 -0300

    feat: Add MAPS_NAME constant for game maps
    This commit adds the MAPS_NAME constant to the `mapas.py` file. The constant is a `Literal` type that represents the names of different game maps. It includes names such as "Vale das Variaveis", "Floresta de Condições", "Montanha dos Loops", "Deserto de Funções", and "Oceano de Objetos". This addition enhances the game functionality by providing a predefined list of map names to be used in the game.

---
+++ src/entities/maps/mapas.py
@@ -0,0 +1,9 @@
+from typing import Literal
+
+MAPS_NAME = Literal[
+    "Vale das Variaveis",
+    "Floresta de Condições",
+    "Montanha dos Loops",
+    "Deserto de Funções",
+    "Oceano de Objetos",
+]
