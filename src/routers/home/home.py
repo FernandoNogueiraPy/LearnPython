cadfd9b797e9672eb3af322e25af3ae1e4735621
Fernando Nogueira Costa <115093658+FernandoNogueiraPy@users.noreply.github.com>
2024-09-07 23:06:23 -0300

    refactor: Update home.py router to include response model for challenge overview

--- src/routers/home/home.py
+++ src/routers/home/home.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Security
 from fastapi.requests import Request
 
+from src.entities.challenges.challenge_overview import ChallengeOverview
 from src.controllers.home.controller_home import ControllerChallengeOverview
 from src.security.auth import AuthSecurity
 
@@ -8,8 +9,10 @@ from src.security.auth import AuthSecurity
 router_challenge_overview = APIRouter(dependencies=[Security(AuthSecurity())])
 
 
-@router_challenge_overview.get("/challenge_overview", tags=["CHALLENGE OVERVIEW"])
-async def challenge_overview(request: Request):
+@router_challenge_overview.get(
+    "/challenge_overview", tags=["CHALLENGE OVERVIEW"], response_model=ChallengeOverview
+)
+async def challenge_overview(request: Request) -> ChallengeOverview:
     return ControllerChallengeOverview().get_challenge_overview(
         request.state.user.id_player, request.state.user.username
     )
