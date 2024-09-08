3ef45e41b4fd739c76ac4a5b3a59a770161e72d3
Fernando Nogueira Costa <115093658+FernandoNogueiraPy@users.noreply.github.com>
2024-09-07 23:06:40 -0300

    refactor: Update main.py to include basic authentication middleware

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 
+from src.security.auth_document import BasicAuthMiddleware
+from src.config import LOGIN_DOCUMENTATION, PASSWORD_DOCUMENTATION
+
 from src.routers import router
 
+
 app = FastAPI()
+
+app.add_middleware(
+    BasicAuthMiddleware, username=LOGIN_DOCUMENTATION, password=PASSWORD_DOCUMENTATION
+)
 app.include_router(router)
 
 HOST = "0.0.0.0"
