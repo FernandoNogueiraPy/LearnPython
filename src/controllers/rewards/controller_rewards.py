ef2ae2369ad45afe1d54b3a866c43eb51319b69b
Fernando Nogueira Costa <115093658+FernandoNogueiraPy@users.noreply.github.com>
2024-09-07 23:06:06 -0300

    refactor: Remove unused imports and commented code

--- src/controllers/rewards/controller_rewards.py
+++ src/controllers/rewards/controller_rewards.py
@@ -1,5 +1,5 @@
-from src.entities.rewards.reward_challenge import RewardsChallenge
-from src.entities.challenges.challenge_overview import ChallengeOverview
+# from src.entities.rewards.reward_challenge import RewardsChallenge
+# from src.entities.challenges.challenge_overview import ChallengeOverview
 
 
 class ControllerRawards:
--- src/controllers/users/controller_user.py
+++ src/controllers/users/controller_user.py
@@ -1,13 +1,28 @@
 from addemongo import QueryBuilder
 from fastapi import HTTPException
 from bcrypt import hashpw, gensalt, checkpw
-
+from uuid import uuid4
+from re import match
 
 from src.core.crypt_helper import CryptHelper
 from src.repositories.users.connect_users import respository_users_sync
 from src.entities.users.register_user import RegisterUser
 from src.entities.users.login_user import LoginUser
 from src.entities.users.auth_user import AuthUser
+from src.entities.users.response_auth import ReponseAuth
+
+
+from typing import ClassVar
+
+
+class EmailValidator:
+    email_pattern: ClassVar[str] = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+    @classmethod
+    def is_email(cls, value: str) -> bool:
+        if match(cls.email_pattern, value):
+            return True
+        return False
 
 
 class ControllerPassword:
@@ -23,7 +38,7 @@ class ControllerPassword:
 
 
 class UserControler:
-    def register_user(self, form_register: RegisterUser):
+    def register_user(self, form_register: RegisterUser) -> ReponseAuth:
         querybuild = QueryBuilder()
         querybuild.set_equal("email", form_register.email)
         querybuild.set_equal("username", form_register.username)
@@ -43,7 +58,8 @@ class UserControler:
                 )
 
         try:
-            new_user = LoginUser(**form_register.model_dump())
+            new_id_player = str(uuid4())
+            new_user = LoginUser(id_player=new_id_player, **form_register.model_dump())
             new_user.password = ControllerPassword.hash_password(form_register.password)
 
             respository_users_sync.insert_one(new_user)
@@ -52,12 +68,11 @@ class UserControler:
                 payload={"player_id": new_user.id_player, "username": new_user.username}
             )
 
-            response = {
-                "player_id": new_user.id_player,
-                "access_token": acess_token,
-                "token_type": "bearer",
-                "message": "Usuário registrado com sucesso!",
-            }
+            response = ReponseAuth(
+                player_id=new_user.id_player,
+                access_token=acess_token,
+                message="Usuário registrado com sucesso!",
+            )
 
             return response
 
@@ -66,9 +81,15 @@ class UserControler:
                 status_code=500, detail=f"Erro ao registrar usuário: {str(e)}"
             )
 
-    def authenticate_user(self, form_auth: AuthUser):
+    def authenticate_user(self, form_auth: AuthUser) -> ReponseAuth:
         querybuild = QueryBuilder()
-        querybuild.set_equal("username", form_auth.username)
+
+        validate_email = EmailValidator.is_email(form_auth.username_or_email)
+
+        if validate_email:
+            querybuild.set_equal("email", form_auth.username_or_email)
+        else:
+            querybuild.set_equal("username", form_auth.username_or_email)
 
         user = respository_users_sync.find_one(querybuild)
 
@@ -77,16 +98,14 @@ class UserControler:
         ):
             raise HTTPException(status_code=400, detail="Erro: Credenciais inválidas.")
 
-        # Gerar token JWT
         acess_token = CryptHelper().encoder(
             payload={"player_id": user.id_player, "username": user.username}
         )
 
-        response = {
-            "player_id": user.id_player,
-            "access_token": acess_token,
-            "token_type": "bearer",
-            "message": "Usuário autenticado com sucesso!",
-        }
+        response = ReponseAuth(
+            player_id=user.id_player,
+            access_token=acess_token,
+            message="Usuário autenticado com sucesso!",
+        )
 
         return response
